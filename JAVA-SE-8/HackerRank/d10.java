7ce3032909999c5fd77fb58a964819391001f850
Ivan Felipe Villarroel <felipevillarroel88@gmail.com>
2020-02-20 09:47:00 -0300

    Se agrega respuesta dia 10, 30 dias de Java, HackerRank

---
+++ JAVA-SE-8/HackerRank/d10.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static int toBit(int num) {
+        StringBuilder result = new StringBuilder();
+        while(num > 0) {
+            int r = num % 2;
+            num /= 2;
+            result.append(String.valueOf(r));
+        }
+        String binario = result.reverse().toString();
+        int respuesta=1;
+        for(int i = 0; i < binario.length() ; i++) { 
+            try {
+                if(binario.charAt(i)=='1' & binario.charAt(i)==binario.charAt(i+1)) {
+                    respuesta++;
+                }
+            }
+            catch(Exception e) {
+            }
+        }
+        return respuesta; 
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        System.out.println(toBit(n));
+        scanner.close();
+    }
+}
+
